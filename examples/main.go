fad1d9d7e36aafde7022c9276434d8906c851f1e
ehsandavari <ehsandavari.ir@gmail.com>
2023-07-20 21:34:21 +0330

    add example

---
+++ examples/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"errors"
+	contextplus "github.com/ehsandavari/go-context-plus"
+	"github.com/ehsandavari/go-tracer"
+	"github.com/google/uuid"
+	"time"
+)
+
+func main() {
+
+	ctx := contextplus.NewContext(context.Background())
+	ctx.SetValue("test", "test_value")
+	ctx.SetRequestId(uuid.New().String())
+	ctx.SetTraceId(uuid.New().String())
+	ctx.User.SetId(uuid.New())
+	ctx.User.SetPhoneNumber("989215580690")
+
+	newTracer := tracer.NewTracer(
+		true,
+		true,
+		false,
+		"localhost",
+		"6831",
+		12,
+		"test",
+		"sConfig.Service.Namespace",
+		"sConfig.Service.InstanceId",
+		"sConfig.Service.Version",
+		"sConfig.Service.Mode",
+		"sConfig.Service.CommitId",
+	)
+
+	span := newTracer.Tracer("asdasdsd")
+	ctx = span.Start(ctx, "create")
+	span.SetString("1", "1")
+	span.SetString("2", "2")
+	span.SetStatus(tracer.Error, "asdfasdfasdfadsfasdfasdfasdfadsf")
+	span.RecordError(errors.New("asdfasdfasdfadsf"))
+	span.End()
+
+	time.Sleep(10 * time.Second)
+}
